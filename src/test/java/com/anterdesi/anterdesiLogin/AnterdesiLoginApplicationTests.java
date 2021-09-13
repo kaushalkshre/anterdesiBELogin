ed38593a2c1cc622b1a86fa09107c6e27b861730
kaushal <kaushal.kishore@tarento.com>
2021-09-13 21:48:06 +0530

    adding src code

---
+++ src/test/java/com/anterdesi/anterdesiLogin/AnterdesiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anterdesi.anterdesiLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnterdesiLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
